385e3c515cb1d35f793a030c39d7bba68b089e1c
Mike Lopez <mikeglopez.91@gmail.com>
2019-01-10 11:37:23 -0600

    Complete HoneyMakerBee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,16 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor() {
+    super();
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+  }
+
+  makeHoney (honeyPot) {
+    this.honeyPot++;
+  };
+
+  giveHoney (honeyPot) {
+    this.honeyPot--;
+  };
 };
